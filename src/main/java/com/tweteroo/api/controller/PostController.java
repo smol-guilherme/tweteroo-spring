0a044710db5764d884cae865e09a5675798ed871
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2023-01-18 17:15:54 -0300

    feat: added list all posts. Further implementation needed for paging functionality .

--- src/main/java/com/tweteroo/api/controller/PostController.java
+++ src/main/java/com/tweteroo/api/controller/PostController.java
@@ -1,9 +1,13 @@
 package com.tweteroo.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.PostDTO;
@@ -21,6 +25,11 @@ public class PostController {
   public String create(@RequestBody PostDTO req) {
     repository.save(new Post(req));
     return "OK";
+  }
+
+  @GetMapping
+  public List<Post> listPage(@RequestParam(required = false) int page) {
+    return repository.findAll();
   }
 
 }
