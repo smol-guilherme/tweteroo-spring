f5c2d8f7d4fe7477a2125595c3b6e2270aac2f2e
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2023-01-18 16:27:03 -0300

    feat: added Post Controller file with a basic POST method.

---
+++ src/main/java/com/tweteroo/api/controller/PostController.java
@@ -0,0 +1,24 @@
+package com.tweteroo.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo.api.dto.PostDTO;
+
+@RestController
+@RequestMapping("/api/post")
+public class PostController {
+
+  @Autowired
+  private PostRepository repository;
+
+  @PostMapping
+  private String create(@RequestBody PostDTO req) {
+    repository.save(new Post(req));
+    return "OK";
+  }
+
+}
