0186341516ba209d3e60a934c560f2d445416c29
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2023-01-24 10:58:01 -0300

    feat: added List user info on /:id Get ROUTE.

--- src/main/java/com/tweteroo/api/controller/PostController.java
+++ src/main/java/com/tweteroo/api/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.dto.PostDTO;
+import com.tweteroo.api.dto.UserDTO;
 import com.tweteroo.api.model.Post;
+import com.tweteroo.api.model.User;
 import com.tweteroo.api.repository.PostRepository;
 
 @RestController
@@ -30,5 +33,10 @@ public class PostController {
   @GetMapping
   public List<Post> listPage(@RequestParam(required = false) int page) {
     return repository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public List<Post> listPostsByUsername(@PathVariable Long id) {
+    return repository.findAllById(id);
   }
 }
